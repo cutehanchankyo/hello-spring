a9025e696b5398db230a6bfc0efd68168aa237d5
cutehanchankyo <jeongchangyo71@gmail.com>
2022-12-14 23:42:44 +0900

    add Jdbc

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -4,11 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.web.bind.annotation.DeleteMapping;
-
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Setter
 @Getter

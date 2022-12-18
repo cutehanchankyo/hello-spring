0adb0a4300cb95fa6d1771b581ab96b71f88bed3
cutehanchankyo <jeongchangyo71@gmail.com>
2022-12-18 22:45:27 +0900

    add Data Jpa

--- src/main/java/hello/hellospring/controller/PostController.java
+++ src/main/java/hello/hellospring/controller/PostController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/post")
+@RequestMapping("post")
 public class PostController {
     @GetMapping("all")
     public Post findAll() {
